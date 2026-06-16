157bb410b03b12a07a886894c407a214aa481569
Laxmi Narasimha Bolla <bollanarasimha3@gmail.com>
2026-06-16 14:34:54 +0530

    Integrate User API with CRUD operations - DAY-3

--- src/main/java/net/engineeringdigest/journalApp/entity/JournalEntry.java
+++ src/main/java/net/engineeringdigest/journalApp/entity/JournalEntry.java
@@ -1,20 +1,27 @@
 package net.engineeringdigest.journalApp.entity;
 
 import lombok.Data;
+import lombok.NonNull;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Document(collection = "journalentry")
 @Data
 public class JournalEntry {
     @Id
     private ObjectId id;
+    @NonNull
     private String title;
     private String content;
     private LocalDateTime date;
 
+    @DBRef
+    private List<JournalEntry> journalEntries = new ArrayList<>();
 
 }
