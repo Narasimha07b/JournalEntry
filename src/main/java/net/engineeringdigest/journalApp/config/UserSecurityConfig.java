2b59f30d179e022f59edbf4d4727888565be4e90
Laxmi Narasimha Bolla <bollanarasimha3@gmail.com>
2026-06-23 11:40:57 +0530

    Implemented Spring Security Configuration and Authentication.

---
+++ src/main/java/net/engineeringdigest/journalApp/config/UserSecurityConfig.java
@@ -0,0 +1,20 @@
+package net.engineeringdigest.journalApp.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class UserSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/hello").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin();
+    }
+}
